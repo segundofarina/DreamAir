47775eead7139b566835da285b5e38ab4b4d79ed
Florencia Cavallin <fcavallin@itba.edu.ar>
2017-06-24 19:35:20 -0300

    favorites single

--- app/src/main/java/itba/dreamair2/fragments/FlightDetailFragment.java
+++ app/src/main/java/itba/dreamair2/fragments/FlightDetailFragment.java
@@ -3,7 +3,6 @@ package itba.dreamair2.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
